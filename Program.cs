d831232b5e0701e6522cb63948d6a07328ab916e
ST10434031-Cameron-Naidoo <st10434031@myemeris.edu.za>
2026-03-02 11:05:39 +0200

    greet() implemented in Voice class

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Speech.Synthesis;
+
+namespace Sekuriti
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Initialize new instance of speech synthesizer
+            Voice voiceOver = new Voice();
+            voiceOver.greet();
+        }
+    }
+}
---
+++ Voice.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech.Synthesis;
+
+namespace Sekuriti
+{
+    class Voice
+    {
+        public SpeechSynthesizer sekuritiVoiceover;
+
+        public Voice()
+        {
+            //Initialize new instance of speech synthesizer
+            sekuritiVoiceover = new SpeechSynthesizer();
+            //configure the audio output
+            sekuritiVoiceover.SetOutputToDefaultAudioDevice();
+        }
+        public void greet()
+        {
+            //Welcome the user
+            sekuritiVoiceover.Speak("Hi there my name is security. your local cybersecurity chatbot. I'm here to help you stay sharp on Mzansi's digital streets. Ask me about scams, phishing, safe passwords, and more.");
+            
+        }
+
+
+    }
+}
